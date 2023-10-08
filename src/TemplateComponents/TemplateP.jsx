35f71b1fa0b4ca09908ca131b14c7d119c4e9a83
Manas0410 <manasshrivastava0410@gmail.com>
2023-10-07 19:39:45 -0700

    image added

--- src/TemplateComponents/TemplateP.jsx
+++ src/TemplateComponents/TemplateP.jsx
@@ -1,18 +1,14 @@
 //rn creating only one template so, will hardcode it later can fetch api on this page for templates
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 const TemplateP = () => {
   return (
     <div>
       <h1>RESUME TEMPLATES</h1>
       <div>
         <Link to={"/create"}>
-          <img
-            src="../../public/images/template1.png"
-            alt=""
-            height={"285px"}
-          />
+          <img src="./template1.PNG" alt="" height={"285px"} />
+          <span>FIRST TEMPLATE</span>
         </Link>
-        <span>FIRST TEMPLATE</span>
       </div>
       <h1>RESUME TEMPLATES</h1>
       <h1>RESUME TEMPLATES</h1>
@@ -34,6 +30,6 @@ const TemplateP = () => {
       <h1>RESUME TEMPLATES</h1>
     </div>
   );
-}
+};
 
-export default TemplateP
+export default TemplateP;
